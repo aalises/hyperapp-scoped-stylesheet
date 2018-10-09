f3bc9ebb93c8e6cb5416d0ee8c81df782543feb0
Albert Alises <albert.alises@gmail.com>
2018-10-09 13:00:44 +0200

    Fixed tests. Passing again.

--- test/main.spec.js
+++ test/main.spec.js
@@ -1,35 +1,28 @@
-const { h } = require("hyperapp")
+const { h, app} = require("hyperapp")
 const { stylesheet } = require("../dist/hyperapp-scoped-stylesheet.umd")
-const decorate = (target, css) => {
-  Object.defineProperty(target.prototype, "render", stylesheet(css)(target, "render", Object.getOwnPropertyDescriptor(target.prototype, "render")))
-}
-const scrap = document.createElement("section")
 
 const style = `
   span { color: red }
 `
 
-const hscript = () => h(
-  "div",
-  null,
-  h("span",null,"some test")
-)
-
-class TestComponent extends Component {
- render() { return hscript() }
-}
-
-decorate(TestComponent, style)
-
-const FunctionalComp = stylesheet(style, props => hscript())
-
 describe("Test several rules regarding stylesheet rendering", () => {
 
-  afterEach(() => scrap.innerHTML = "")
+  beforeEach(() => {
+    document.body.innerHTML = ""
+  });
 
-  it("should render a functional view", () => {
-    render(h(FunctionalComp, null, "stuff"), scrap)
-    expect(scrap.innerHTML).toContain("span { color: red }")
-    expect(scrap.innerHTML).toContain("style scoped")
+  it("should render a functional view", done => {
+   
+    const view = () => (
+      <div oncreate={() => {
+        expect(document.body.innerHTML).toContain("span { color: red }");
+        expect(document.body.innerHTML).toContain(`style scoped`);
+        done()
+      }}>
+        <span> Text to be styled</span>
+      </div>
+    )
+
+    app(null, null, stylesheet(style, view), document.body);
   })
 })
